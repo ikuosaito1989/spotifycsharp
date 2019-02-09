5c20ef6d3c9b86303563879b374cfaf79740b4fe
斉藤育男 <ikuo@saitouikuonoMacBook-ea.local>
2019-02-10 00:24:09 +0900

    add Search

--- Program.cs
+++ Program.cs
@@ -4,23 +4,32 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 using Newtonsoft.Json;
+using spotifycsharp.model;
 
 namespace spotifycsharp
 {
     class Program
     {
+
         public static async Task Main(string[] args)
         {
             var clientId = "";
             var clientSecret = "";
-            var token = await new SpotifyApi().GetSpotifyToken(clientId, clientSecret);
+            var spotify = new SpotifyApi(clientId, clientSecret);
+            var searchArtists = await spotify.GetSearchToken("ariana grande");
         }
     }
 
     public class SpotifyApi
     {
         private static HttpClient _client = new HttpClient();
-        public async Task<string> GetSpotifyToken(string clientId, string clientSecret)
+        private string _accessToken;
+        public SpotifyApi(string clientId, string clientSecret)
+        {
+            _accessToken = GetSpotifyToken(clientId, clientSecret).Result;
+        }
+
+        private async Task<string> GetSpotifyToken(string clientId, string clientSecret)
         {
             var clientKey = Convert.ToBase64String(Encoding.UTF8.GetBytes($"{clientId}:{clientSecret}"));
             // Create a request.
@@ -36,6 +45,25 @@ namespace spotifycsharp
             var json = await response.Content.ReadAsStringAsync();
             var obj = JsonConvert.DeserializeObject<dynamic>(json);
             return (string)obj.access_token;
+        }
+
+        public async Task<SearchArtists> GetSearchToken(string query, string limit = "1", string offset = "0")
+        {
+            var parame = new Dictionary<string, string>()
+            {
+                { "q", query },
+                { "type", "artist" },
+                { "market", "US" },
+                { "limit", limit },
+                { "offset", offset },
+            };
+            var parameters = await new FormUrlEncodedContent(parame).ReadAsStringAsync();
+            var request = new HttpRequestMessage(HttpMethod.Get, $"https://api.spotify.com/v1/search?{parameters}");
+            request.Headers.Add("ContentType", "application/json");
+            request.Headers.Add("Authorization", $"Bearer {this._accessToken}");
+            var response = await _client.SendAsync(request);
+            var json = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<SearchArtists>(json);
         }
     }
 }
